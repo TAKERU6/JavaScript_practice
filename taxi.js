a94288144d9ec2f75992bff0937b009989c7f7df
admim <admim@admims-MBP.fritz.box>
2020-07-24 16:09:56 +0200

    new_taxi

--- taxi.js
+++ taxi.js
@@ -4,10 +4,14 @@
 const distance = 4530;
 
 const taxiPrice = (distance) => {
-  if (distance < 1279) return 610;
-  const addDistance = distance - 1000;
+  const FIRST_FARE = 610;
+  const FIRST_FARE_DISTANCE = 1000;
+  const ADD_FEE = 60;
+  const ADD_1METER_DISTANCE = 280;
+  if (distance < 1280) return FIRST_FARE;
+  const addDistance = distance - FIRST_FARE_DISTANCE;
   const addPrice = Math.floor(addDistance / 280) * 60;
-  const totalPrice = addPrice + 610;
+  const totalPrice = addPrice + FIRST_FARE;
   return totalPrice;
 };
 
